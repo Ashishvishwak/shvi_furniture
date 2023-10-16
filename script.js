6bc4d674ab48eef6ef8e6d809efba1ad4646c834
Ashishvishwak <126412991+Ashishvishwak@users.noreply.github.com>
2023-10-16 18:22:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let tl=gsap.timeline()
+
+tl.from(".header,.content,.links li,.search-icon",{
+    y:-100,
+    duration:1,
+    delay:1,
+    stagger:0.5,
+    opacity:0,
+
+
+
+});
+// tl.to("links",{
+//     y:100,
+//     duration:1,
+//     delay:1,
+    
+
+
+
+// });
